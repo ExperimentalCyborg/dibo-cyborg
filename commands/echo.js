ca1a0b9f954cae80aeec1f2496af22372128ed9a
ExperimentalCyborg <cuterobot@protonmail.ch>
2020-12-09 14:31:10 +0100

    echo command

---
+++ commands/echo.js
@@ -0,0 +1,14 @@
+const dibo = require('../libs/dibo');
+
+module.exports = {
+    'names': ['echo', 'say', 'repeat'],
+    'privilege': dibo.privilege.ADMIN,
+    'summary': 'Make me say something.',
+    'help': '',
+    'func': async (priv, msg, args) => {
+        msg.delete().
+            catch(reason => dibo.log.error('Failed to delete echo command message', reason, msg.guild.id));
+        msg.channel.send(args.join(' ')).
+            catch(reason => dibo.log.error('Failed to send echo message', reason, msg.guild.id));
+    }
+}
