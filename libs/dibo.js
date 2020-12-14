0158b19224165a9d24a5f39c20d2ed423daf99f6
ExperimentalCyborg <cuterobot@protonmail.ch>
2020-12-14 23:31:06 +0100

    Revert "Removed useless return"
    This reverts commit 2ab24508e45f7789de84d5e65424e2cf4a9e01d6.

--- libs/dibo.js
+++ libs/dibo.js
@@ -70,6 +70,7 @@ function loadCommands() {
             if (settings.debug) {
                 throw reason;
             }
+            return;
         }
     });
 }
--- test.js
+++
@@ -1,6 +0,0 @@
-const tools = require('./libs/tools');
-
-console.log(tools.durationToText(1000 * 60 * 60 * 24 * 7 + 126345));
-console.log(1000 * 60 * 60 * 24)
-
-console.log(tools.durationToText(191625));
