266399764d2a0ab1e86c72b5c01fc9faacd22ff0
ExperimentalCyborg <cuterobot@protonmail.ch>
2020-12-18 15:11:54 +0100

    banning

---
+++ commands/ban.js
@@ -0,0 +1,44 @@
+const dibo = require('./../libs/dibo');
+
+module.exports = {
+    'names': ['ban'],
+    'privilege': dibo.privilege.MOD,
+    'summary': 'Ban a user, temporarily or permanently.',
+    'help': '`%%pban <user> [duration] [reason]`\n\n' +
+        'Duration must be a whole number. Examples:\n' +
+        '`%%pban baduser#1234 30 my reason here` will ban for 30 minutes.\n' +
+        '`%%pban baduser#1234 2h my reason here` will ban for 2 hours.\n' +
+        '`%%pban baduser#1234 14d my reason here` will ban for 14 days.\n' +
+        '`%%pban baduser#1234 perm my reason here` will ban permanently.\n' +
+        '\nAny invalid duration is automatically a permanent ban. That means this also results in a permanent ban:\n' +
+        '`%%pban baduser#1234 forever my reason here`',
+    'func': async (priv, msg, args, userText = '', durationText = '') => {
+        //msg.delete(); //todo ninja mode
+
+        if(!userText){
+            return dibo.commandHandler.run(msg, priv, 'help', ['ban']);
+        }
+
+        let user = await dibo.tools.textToMember(msg.guild, userText);
+        if(!user){
+            return false;
+        }
+
+        durationText = durationText.toLowerCase();
+        let duration;
+        if(durationText.endsWith('h')){
+            durationText = durationText.slice(0, -1);
+            duration = parseInt(durationText) * 60;
+        }else if(durationText.endsWith('d')){
+            durationText = durationText.slice(0, -1);
+            duration = parseInt(durationText) * 60 * 24;
+        }else if(durationText.endsWith('m')){
+            durationText = durationText.slice(0, -1);
+            duration = parseInt(durationText)
+        }else{
+            duration = parseInt(durationText)
+        }
+
+        return await dibo.cyborg.moderation.ban(msg.author, user, duration, args.slice(2).join(' ') || undefined);
+    }
+}
---
+++ commands/unban.js
@@ -0,0 +1,20 @@
+const dibo = require('./../libs/dibo');
+
+module.exports = {
+    'names': ['unban', 'pardon'],
+    'privilege': dibo.privilege.MOD,
+    'summary': 'Undo a ban.',
+    'help': '`%%punban <user> [reason]`',
+    'func': async (priv, msg, args, userText) => {
+        if(!userText){
+            return dibo.commandHandler.run(msg, priv, 'help', ['unban']);
+        }
+
+        let user = await dibo.tools.textToMember(msg.guild, userText);
+        if(!user){
+            return false;
+        }
+
+        return await dibo.cyborg.moderation.unban(msg.author, user, args.slice(1).join(' ') || undefined);
+    }
+}
