9c0f4b9f4d281a8b7411a3c85c7949efe57cdbd5
ExperimentalCyborg <cuterobot@protonmail.ch>
2020-12-18 15:12:15 +0100

    warning

---
+++ commands/warn.js
@@ -0,0 +1,20 @@
+const dibo = require('./../libs/dibo');
+
+module.exports = {
+    'names': ['warn', 'warning'],
+    'privilege': dibo.privilege.MOD,
+    'summary': 'Warn a user, and add it to their record.',
+    'help': '`%%pwarn <user> <reason>`',
+    'func': async (priv, msg, args, userText) => {
+        if(!userText){
+            return dibo.commandHandler.run(msg, priv, 'help', ['warn']);
+        }
+
+        let user = await dibo.tools.textToMember(msg.guild, userText);
+        if(!user){
+            return false;
+        }
+
+        return await dibo.cyborg.moderation.warn(msg.author, user, args.slice(1).join(' ') || undefined);
+    }
+}
