5dd2d271d49ea44e675692821851a5276883e8f1
ExperimentalCyborg <cuterobot@protonmail.ch>
2021-02-12 16:16:55 +0100

    tool for rebuilding a sentence from a command token array

--- libs/tools.js
+++ libs/tools.js
@@ -196,4 +196,14 @@ module.exports = class {
         }
         return paddedText;
     }
+
+    //Do a naive attempt at recombining a list of command arguments into a string, including quotes.
+    static textRecombine(arr){
+        arr.forEach((value, index) => {
+            if(value.indexOf(' ') >= 0){
+                arr[index] = `"${value}"`;
+            }
+        });
+        return arr.join(' ');
+    }
 }
